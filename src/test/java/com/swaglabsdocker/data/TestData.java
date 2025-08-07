a2c50edabaf8fb5971051d9224548d788b8b35e8
Kwame Oduro <odurokwameee@gmail.com>
2025-08-07 16:08:25 +0000

    feat: add test data class for managing test input values

---
+++ src/test/java/com/swaglabsdocker/data/TestData.java
@@ -0,0 +1,18 @@
+package com.swaglabsdocker.data;
+
+
+public class TestData {
+
+    // Valid user
+    public static final String VALID_USERNAME = "standard_user";
+    public static final String VALID_PASSWORD = "secret_sauce";
+
+    // Invalid users
+    public static final String INVALID_USERNAME = "invalid_user";
+    public static final String INVALID_PASSWORD = "wrong_password";
+
+    // Checkout info
+    public static final String FIRST_NAME = "John";
+    public static final String LAST_NAME = "Doe";
+    public static final String POSTAL_CODE = "12345";
+}
